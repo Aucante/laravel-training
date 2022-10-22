6505036159f02a31a4c9c3817f8071485f58a5a0
Aucante <sebastien.aucante@gmail.com>
2022-10-22 13:03:04 +0200

    Clean

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', [MainController::class, 'index']);
-Route::resource('articles', ArticleController::class);
-
-
