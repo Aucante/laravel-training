52b75f6be81d834b68649399b1540d1377581026
Aucante <sebastien.aucante@gmail.com>
2022-10-23 12:31:04 +0200

    bootstrap install

--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -1,6 +1,20 @@
-<ul>
-    <li>test</li>
-    <li>test</li>
-    <li>test</li>
-    <li>test</li>
-</ul>
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
