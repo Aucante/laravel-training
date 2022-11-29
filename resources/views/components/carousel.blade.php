bafac9cfbb808c3ddc9ed1f181db482ad6f7e29e
Aucante <sebastien.aucante@gmail.com>
2022-11-29 21:47:11 +0100

    Carousel + media admin

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,41 +1,55 @@
-
-<div class="container-fluid g-0">
-    <div class="card text-white rounded-0 border-0 display-main-1">
-        <img
-            src="{{ asset('carousel7.jpg') }}"
-            class="card-img"
-            alt="homepage"
-        />
-        <div class="d-flex align-items-center justify-content-center card-img-overlay rounded-0">
-            <div class="text-center mt-4">
-                <div
-                    class="card-body d-flex align-items-center justify-content-center"
-                >
-                    <div class="card-body d-flex align-items-center justify-content-center pt-5">
-                        <h1 class="display-4 text-uppercase color-title">Discover your home</h1>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ asset('carousel5.jpg') }}" class="d-block w-100" alt="carousel1">
+            <div class="d-flex align-items-center justify-content-center card-img-overlay rounded-0">
+                <div class="text-center mt-4">
+                    <div
+                        class="card-body d-flex align-items-center justify-content-center"
+                    >
+                        <div class="card-body d-flex align-items-center justify-content-center pt-5">
+                            <h1 class="display-4 text-uppercase text-light">Nutrition Optimization</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('carousel6.jpg') }}" class="d-block w-100" alt="carousel2">
+            <div class="d-flex align-items-center justify-content-center card-img-overlay rounded-0">
+                <div class="text-center mt-4">
+                    <div
+                        class="card-body d-flex align-items-center justify-content-center"
+                    >
+                        <div class="card-body d-flex align-items-center justify-content-center pt-5">
+                            <h1 class="display-4 text-uppercase text-light">Discover your home</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('carousel6.jpg') }}" class="d-block w-100" alt="carousel3">
+            <div class="d-flex align-items-center justify-content-center card-img-overlay rounded-0">
+                <div class="text-center mt-4">
+                    <div
+                        class="card-body d-flex align-items-center justify-content-center"
+                    >
+                        <div class="card-body d-flex align-items-center justify-content-center pt-5">
+                            <h1 class="display-4 text-uppercase text-light">Discover your home</h1>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Responsive -->
-{{--    <div class="card text-white rounded-0 border-0 display-main-2">--}}
-{{--        <img--}}
-{{--            src="#"--}}
-{{--            class="card-img"--}}
-{{--            alt="homepage"--}}
-{{--            style="min-height: 300px"--}}
-{{--        />--}}
-{{--        <div class="d-flex align-items-center justify-content-center card-img-overlay rounded-0">--}}
-{{--            <div class="text-center">--}}
-{{--                <div--}}
-{{--                    class="card-body d-flex align-items-center justify-content-center"--}}
-{{--                >--}}
-{{--                    <div class="card-body d-flex align-items-center justify-content-center">--}}
-{{--                        <h1 class="display-3 text-uppercase text-white">Discover your home</h1>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 </div>
+
