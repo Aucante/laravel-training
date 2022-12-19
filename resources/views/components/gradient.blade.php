1a11f0fe549cc1242587ed017c43620b741ffeb5
Aucante <sebastien.aucante@gmail.com>
2022-12-19 19:29:44 +0100

    homepage composant gradient

---
+++ resources/views/components/gradient.blade.php
@@ -0,0 +1,25 @@
+<div class="container-fluid gradientContainerTop"></div>
+<div class="container-fluid gradient-bg">
+    <div class="container py-5">
+        <div class="align-items-center text-center row py-5">
+            <div
+                class="col-6 col-md-6 col-lg-4 offset-lg-1 my-4"
+            >
+                <div data-aos="zoom-in" class="container text-center">
+
+                    <i class="fa-solid fa-dumbbell fa-10x"></i>
+                </div>
+            </div>
+            <div
+                class="col-6 col-md-6 col-lg-4 offset-lg-2 my-4"
+            >
+                <div data-aos="zoom-in" class="container text-center">
+                    <p class="display-4 text-uppercase">
+                        Improve your training level
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid gradientContainerBottom"></div>
