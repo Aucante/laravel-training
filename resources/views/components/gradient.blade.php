f7db17913a9d2fb6f4470556da2e681c6f8ad38c
Aucante <sebastien.aucante@gmail.com>
2022-12-20 19:39:07 +0100

    homepage composant gradient color

--- resources/views/components/gradient.blade.php
+++ resources/views/components/gradient.blade.php
@@ -1,5 +1,5 @@
-<div class="container-fluid gradientContainerTop"></div>
-<div class="container-fluid gradient-bg">
+{{--<div class="container-fluid gradientContainerTop"></div>--}}
+<div class="container-fluid gradient-bg my-5">
     <div class="container py-5">
         <div class="align-items-center text-center row py-5">
             <div
@@ -7,14 +7,14 @@
             >
                 <div data-aos="zoom-in" class="container text-center">
 
-                    <i class="fa-solid fa-dumbbell fa-10x"></i>
+                    <i class="fa-solid fa-dumbbell fa-10x text-light"></i>
                 </div>
             </div>
             <div
                 class="col-6 col-md-6 col-lg-4 offset-lg-2 my-4"
             >
                 <div data-aos="zoom-in" class="container text-center">
-                    <p class="display-4 text-uppercase">
+                    <p class="display-5 text-uppercase text-light">
                         Improve your training level
                     </p>
                 </div>
@@ -22,4 +22,4 @@
         </div>
     </div>
 </div>
-<div class="container-fluid gradientContainerBottom"></div>
+{{--<div class="container-fluid gradientContainerBottom"></div>--}}
