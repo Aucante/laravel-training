bf6cf09941cd278afd274cad92a48b7af2a848b9
Aucante <sebastien.aucante@gmail.com>
2023-01-18 23:41:00 +0100

    update newsletter

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -1,9 +1,8 @@
 <div class="container py-5">
     <div class="row">
         <div class="col-lg-6 offset-lg-3 col-xxl-4 offset-xxl-4">
-            <div class="card text-bg-dark">
-                <img src="{{ asset('media1.jpg') }}" class="card-img" alt="newsletter">
-                <div class="card-img-overlay">
+            <div class="card text-center"
+                <div class="card-body">
                     <h5 class="card-title text-center display-5 mt-4">Newsletter</h5>
                     <p class="card-text text-center display-6 fs-5 mt-4">This content is a little bit longer.</p>
                     <div class="text-center mt-4">
