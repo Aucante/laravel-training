9d483b3ec3bb8a499fbefa696dcac9d7625ab514
Aucante <sebastien.aucante@gmail.com>
2023-02-12 18:21:54 +0100

    Newsletter template

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -1,15 +1,21 @@
-<div class="container py-5">
-    <div class="row">
-        <div class="col-lg-6 offset-lg-3 col-xxl-4 offset-xxl-4">
-            <div class="card text-center"
-                <div class="card-body">
-                    <h5 class="card-title text-center display-5 mt-4">Newsletter</h5>
-                    <p class="card-text text-center display-6 fs-5 mt-4">This content is a little bit longer.</p>
-                    <div class="text-center mt-4">
-                        <button class="btn btn-light text-center disabled">Coming soon</button>
+<section class="subscription bg-white py-5">
+    <div class="container py-5">
+        <div class="row py-4">
+            <div class="col-lg-12">
+                <div class="subscription-wrapper">
+                    <div class="d-flex subscription-content justify-content-between align-items-center flex-column flex-md-row text-center text-md-left">
+                        <h3 class="flex-fill">Subscribe <br> to our newsletter</h3>
+                        <form action="#" class="row flex-fill">
+                            <div class="col-lg-7 my-md-2 my-2">
+                                <input type="email" class="form-control px-4 border-0 w-100 text-center text-md-left" id="email" placeholder="Email" name="email" disabled>
+                            </div>
+                            <div class="col-lg-5 my-md-2 my-2">
+                                <a href="#"><button type="submit" class="btn btn-primary border-0">COMING SOON</button></a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
