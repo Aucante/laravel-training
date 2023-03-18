b167d1761bc2d8b86cdee32b8c3174d97468c6b4
Aucante <sebastien.aucante@gmail.com>
2023-03-18 20:24:29 +0100

    Padding auth page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 
 @section('content')
-<div class="container my-5">
+<div class="container my-5" style="padding-bottom: 150px; padding-top: 100px">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
